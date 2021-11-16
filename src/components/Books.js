0349d3de1c4e73f2d52bc40f91edcdbb722a1484
Kingstalux <kingsleyngu25@gmail.com>
2021-11-16 22:05:47 +0100

    Fix linters errors

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook, removeBook } from '../redux/books/books';
+import { addBook, removeBook } from '../redux/books/book';
 
 export default function Books() {
   const dispatch = useDispatch();
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import booksReducer from './books/books';
+import booksReducer from './books/book';
 
 const reducer = combineReducers({
   booksReducer,
